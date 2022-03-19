48858e28c5f876920edea6866f6223d8718971f6
Hygor Martins <hygor.martins74@gmail.com>
2022-03-19 16:17:11 -0300

    first commit

---
+++ Cara ou Coroa/ResultadoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultadoViewController.swift
+//  Cara ou Coroa
+//
+//  Created by Hygor Martins on 19/03/22.
+//
+
+import UIKit
+
+class ResultadoViewController: UIViewController {
+    
+    var numeroRandomico: Int = 0
+    
+    @IBOutlet weak var moedaImagem: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if numeroRandomico == 0 {//cara
+            
+            moedaImagem.image = UIImage(named: "moeda_cara")
+            
+        }else{//coroa
+            
+            moedaImagem.image = UIImage(named: "moeda_coroa")
+            
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Cara ou Coroa/ViewController.swift
+++ Cara ou Coroa/ViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "jogarMoeda"{
+            let viewControllerDestino = segue.destination as! ResultadoViewController
+            viewControllerDestino.numeroRandomico = Int( arc4random_uniform(2) )
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
