7cc77f413434efe1d63c3174b4dd027b742712fd
Hygor Martins <hygor.martins74@gmail.com>
2022-03-19 15:23:10 -0300

    Initial Commit

---
+++ Cara ou Coroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cara ou Coroa
+//
+//  Created by Hygor Martins on 19/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
